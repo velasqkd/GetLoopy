247a4acc233109b077b8215d6687a39986a0c674
velasqkd <velasqkd@mail.uc.edu>
2025-02-28 17:46:45 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,94 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Random;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class Task1{
+    public static void main(String [] args) {
+    for (int i = 0; i <= 30; i++)
+        {
+            System.out.println(i);
         }
+    }
+}
+
+class Task2 {
+    public static void main(String[] args) {
+    for (int i = 30; i >= 0; i--)
+        {
+        System.out.println(i);
+        }
+    }
+}
+
+class Task3 {
+    public static void main(String[] args) {
+    for (int i = 0; i <= 18; i += 3)
+        {
+        System.out.println(i);
+        }
+    }
+}
+
+class Task4 {
+    public static void main(String[] args) {
+    for (int i = 10; i >= 0; i -= 2)
+        {
+        System.out.println(i);
+        }
+    }
+}
+
+class Task5 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Task6 {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Task7 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++)
+                System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+
+class Task8 {
+    public static void main(String[] args) {
+        Random generator = new Random();
+        int tries = 0;
+        int sum = 0;
+        int roll1, roll2, roll3;
+
+        System.out.println("Roll,Die1, Die2, Die3, Sum");
+        System.out.println("---------------------------");
+
+        do {
+            roll1 = generator.nextInt(6) + 1;
+            roll2 = generator.nextInt(6) + 1;
+            roll3 = generator.nextInt(6) + 1;
+            sum = roll1 + roll2 + roll3;
+            tries++;
+
+            System.out.printf("%-6d\t%-6d\t%-6d\t%-6d\t%-6d\n", tries, roll1, roll2, roll3, sum);
+
+        } while (!(roll1 == roll2 && roll2 == roll3));
+
+        System.out.println("You won after " + tries + " tries.");
     }
 }
